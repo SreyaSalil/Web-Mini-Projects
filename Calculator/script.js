b1ad5a6311f5211886c6267fc431f54b6b409a02
Sreya Salil <44632618+SreyaSalil@users.noreply.github.com>
2021-09-19 20:28:40 +0530

    Calculator Mini Project
    A Calculator made with HTML 5, CSS and JS

---
+++ Calculator/script.js
@@ -0,0 +1,17 @@
+function buttonClick(val){
+    if(val=='X'){
+        document.getElementById("screen").value+='*'
+        return
+    }
+    document.getElementById("screen").value+=val
+}
+
+function clearDisplay(){
+    document.getElementById("screen").value=""
+}
+
+function equalClick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
